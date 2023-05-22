8cf112b13ee85b3d336b24584327e6fe1c23e43c
MariiaVoloshanovska <masterskaiamary@gmail.com>
2023-05-22 19:07:08 +0300

    added esc task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -7,7 +7,7 @@ const galleryForHtml = galleryItems
     ({ original, preview, description }) =>
       `<li class = "gallery__item">
       <a class="gallery__link" href = ${original}>
-      <img src = ${preview} alt = ${description} 
+      <img src = ${preview} alt = '${description}' 
       data-source = ${original} class = "gallery__image"></a></li>`
   )
   .join("");
@@ -19,19 +19,34 @@ const evImage = (e) => {
   if (!e.target.classList.contains("gallery__image")) {
     return;
   }
-
   //open modal window (show) from new galery
-  const instance = basicLightbox.create(`<img src="${e.target.dataset.source}" alt="${e.target.alt}" width="800" height="600">`);
-  instance.show();
-
-  //done close for ESC (close modal window)
-  if (instance.visible()) {
-    newGallery.addEventListener("keydown", (e) => {
-      if (e.code === "Escape") {
-        instance.close();
-      }
-    });
+  const instance = basicLightbox.create(
+    `<img src="${e.target.dataset.source}" 
+  alt="${e.target.alt}" width="800" height="600">`,
+    // видалити "keydown" з вікна
+    {
+      onClose: (instance) => {
+        window.removeEventListener("keydown", сloseWinEscape);
+      },
+    }
+  );
+  // callback (задкриття по кнопці "Esc" )
+  function сloseWinEscape(e) {
+    if (e.code === "Escape") {
+      instance.close();
+    }
+    return;
   }
+  instance.show();
 };
 
 newGallery.addEventListener("click", evImage);
+
+//   // //done close for ESC (close modal window)
+// if (instance.visible()) {
+//   newGallery.addEventListener("keydown", (e) => {
+//     if (e.code === "Escape") {
+//       instance.close();
+//     }
+//   });
+// }
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -7,7 +7,7 @@ const galleryForHtml = galleryItems
     ({ original, preview, description }) =>
       `<li class="gallery__item">
   <a class="gallery__link" href=${original}>
-    <img class="gallery__image" src=${preview} alt=${description} />
+    <img class="gallery__image" src=${preview} alt=' ${description}' />
   </a>
 </li>`
   )
